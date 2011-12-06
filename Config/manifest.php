7b0c7dd6ba5c9a9eed1391d5bb1c2041a5182cd0
arie <asutanto@xintesa.com>
2011-12-06 17:57:37 +0700

    set manifest

---
+++ Config/manifest.php
@@ -0,0 +1,11 @@
+<?php
+
+$pluginManifest = array(
+	'name'=> 'Polling',
+	'description'=> 'Polling plugin for demonstrating hook system',
+
+	'author'=> '',
+	'authorEmail'=> '',
+	'authorUrl'=> '',
+
+	);
