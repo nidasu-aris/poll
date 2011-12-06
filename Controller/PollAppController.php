78333ac7d06e8861c0da7ad71c3032ceb6fbb27c
arie <asutanto@xintesa.com>
2011-12-06 17:54:34 +0700

    set session

--- Controller/PollAppController.php
+++ Controller/PollAppController.php
@@ -1,5 +1,17 @@
 <?php
+
 class PollAppController extends AppController {
 
+	function beforeFilter() {
+		parent::beforeFilter();
+		$this->setupSession();
+	}
+
+	function setupSession() {
+		if (!$this->Session->check('SessionInfo.start')){
+			$this->Session->write('SessionInfo.start', date('Y-m-d H:i:s'));
+			$this->Session->write('SessionInfo.ip', env('REMOTE_ADDR'));
+			$this->Session->write('SessionInfo.user_agent', env('HTTP_USER_AGENT'));
+		}
+	}
 }
-?>
